e8fb7cef516dec0e98d456bbc503d7e577ecbd7e
Searinox <searinox@gmx.de>
2020-05-23 20:35:34 +0200

    +

--- th/main7.js
+++ th/main7.js
@@ -9,6 +9,7 @@
         'lodash': ['https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.min', ],
         'ramda': ['https://cdnjs.cloudflare.com/ajax/libs/ramda/0.25.0/ramda.min', ],
         'sugar': ['https://cdnjs.cloudflare.com/ajax/libs/sugar/2.0.4/sugar.min', ],
+        'particles': ['https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js', ],
     }
 
     const akk = {
@@ -28,23 +29,27 @@
             cfg: {},
             init() {
                 console.log("particle.init")
-                require(["particleCfgTriangle1"], (cfg) => {
-                    console.log("particleCfgTriangle ready", [this, cfg])
-                    this.cfg.triangle = cfg
-                    akk.addDiv(document.body, "particleTriangle")
-                    particlesJS("particleTriangle", this.cfg.triangle);
-                })
-                require(["particleCfgCircle1"], (cfg) => {
-                    console.log("particleCfgCircle ready", [this, cfg])
-                    this.cfg.circle = cfg
-                    akk.addDiv(document.body, "particleCircle")
-                    particlesJS("particleCircle", this.cfg.triangle);
-                })
-                require(["particleCfgStar1"], (cfg) => {
-                    console.log("particleCfgStar ready", [this, cfg])
-                    this.cfg.star = cfg
-                    akk.addDiv(document.body, "particleStar")
-                    particlesJS("particleStar", this.cfg.triangle);
+                require(["particles"], (particlesJS) => {
+                    console.log("particlesJS ready", [this, particlesJS])
+                    //this.cfg.triangle = cfg
+                    require(["particleCfgTriangle1"], (cfg) => {
+                        console.log("particleCfgTriangle ready", [this, cfg])
+                        this.cfg.triangle = cfg
+                        akk.addDiv(document.body, "particleTriangle")
+                        particlesJS("particleTriangle", this.cfg.triangle);
+                    })
+                    require(["particleCfgCircle1"], (cfg) => {
+                        console.log("particleCfgCircle ready", [this, cfg])
+                        this.cfg.circle = cfg
+                        akk.addDiv(document.body, "particleCircle")
+                        particlesJS("particleCircle", this.cfg.triangle);
+                    })
+                    require(["particleCfgStar1"], (cfg) => {
+                        console.log("particleCfgStar ready", [this, cfg])
+                        this.cfg.star = cfg
+                        akk.addDiv(document.body, "particleStar")
+                        particlesJS("particleStar", this.cfg.triangle);
+                    })
                 })
             }
         },
