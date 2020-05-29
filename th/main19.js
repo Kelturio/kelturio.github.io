2d5d801d49986ec3ba43c8106c9dc8d9a40c5007
Searinox <searinox@gmx.de>
2020-05-30 00:19:48 +0200

    +

--- th/main19.js
+++ th/main19.js
@@ -22,7 +22,7 @@
                 console.log("lodash, sugar ready", [this, lodash])
                 this.addImg(document.body, "xkcd", "https://i.imgur.com/8rH20Pn.png")
                 this.addImg(document.body, "hackerman", "https://i.giphy.com/media/RyXVu4ZW454IM/giphy.gif")
-                this.particle.init()
+                //this.particle.init()
                 this.matrix.init()
             })
         },
